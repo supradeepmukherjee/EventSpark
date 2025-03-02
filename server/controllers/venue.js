00a5c26c4041e249ecd445c456a9e905efa83801
Supradeep Mukherjee <supradeep2004@gmail.com>
2025-03-03 00:52:56 +0530

    Updated store Venue Details Logic to store Date in a better human understandable way

--- server/controllers/venue.js
+++ server/controllers/venue.js
@@ -2,10 +2,12 @@ import { tryCatch } from '../middlewares/error.js'
 import { Venue } from '../models/Venue.js'
 
 const store = tryCatch(async (req, res, next) => {
-    const venue = await Venue.create(req.body)
+    const venue = await Venue.create({
+        ...req.body,
+        date: req.body.date.split('-').reverse().join('-')
+    })
     res.status(200).json({ success: true, data: venue, msg: 'Venue Details Stored Successfully' })
 })
 
 
 export { store }
-
