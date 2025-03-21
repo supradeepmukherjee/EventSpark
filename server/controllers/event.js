79d6dfd8c24182d04242161584411543205226f0
Supradeep Mukherjee <supradeep2004@gmail.com>
2025-03-21 16:01:18 +0530

    Get All Events, Get Events by logged in User

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -2,8 +2,8 @@ import { tryCatch } from '../middlewares/error.js'
 import { Event } from '../models/Event.js'
 
 const create = tryCatch(async (req, res, next) => {
-    const event=await Event.create(req.body)
-    res.status(200).json({ success: true, msg: 'Event Created Successfully',event })
+    const event = await Event.create(req.body)
+    res.status(200).json({ success: true, msg: 'Event Created Successfully', event })
 })
 
 const getEventDetails = tryCatch(async (req, res, next) => {
@@ -11,4 +11,15 @@ const getEventDetails = tryCatch(async (req, res, next) => {
     res.status(200).json({ success: true, data: event })
 })
 
-export { create, getEventDetails }
+const allEvents = tryCatch(async (req, res, next) => {
+    const events = await Event.find({})
+    res.status(200).json({ success: true, events })
+})
+
+const eventsByUser = tryCatch(async (req, res, next) => {
+    console.log(req.user)
+    const events = await Event.find({ user: req.user })
+    res.status(200).json({ success: true, events })
+})
+
+export { create, getEventDetails, allEvents, eventsByUser }
--- server/routes/event.js
+++ server/routes/event.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { create, getEventDetails } from '../controllers/event.js'
+import { allEvents, create, eventsByUser, getEventDetails } from '../controllers/event.js'
 
 const app = Router()
 
 app.post('/', create)
 app.get('/', getEventDetails)
+app.get('/all', allEvents)
+app.get('/by-account', eventsByUser)
 
 export default app
