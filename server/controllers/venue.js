5e9509f990ae2a174b25a1378b30c93f6a74e67c
Supradeep Mukherjee <supradeep2004@gmail.com>
2025-04-27 14:31:51 +0530

    Venue Fixed

--- server/controllers/venue.js
+++ server/controllers/venue.js
@@ -1,9 +1,26 @@
 import { tryCatch } from '../middlewares/error.js'
+import { Event } from '../models/Event.js'
 import { Venue } from '../models/Venue.js'
 
 const store = tryCatch(async (req, res, next) => {
+    const events = await Event.find({
+        user: req.user,
+        status: 'Approved'
+    })
+    let event = ''
+    console.log(events)
+    events.forEach(e => {
+        const endDate = new Date(e.end);
+        const today = new Date();
+
+        // Set today's time to 00:00:00 to compare only dates (optional)
+        today.setHours(0, 0, 0, 0);
+
+        if (endDate >= today) event = e._id
+    });
     const venue = await Venue.create({
         ...req.body,
+        event,
         date: req.body.date.split('-').reverse().join('-')
     })
     res.status(200).json({ success: true, data: venue, msg: 'Venue Details Stored Successfully' })
--- server/models/Venue.js
+++ server/models/Venue.js
@@ -19,7 +19,7 @@ const schema = new Schema({
         required: true,
     },
     date: {
-        type: Number,
+        type: String,
         required: true,
     },
     extras: String,
