378802a76b91d4e8dd49a8bdafce46d2b2ff5660
Supradeep Mukherjee <supradeep2004@gmail.com>
2025-04-27 14:28:13 +0530

    Lighting Fixed

--- server/controllers/lighting.js
+++ server/controllers/lighting.js
@@ -1,8 +1,24 @@
 import { tryCatch } from '../middlewares/error.js'
+import { Event } from '../models/Event.js'
 import { Lighting } from '../models/Lighting.js'
 
 const store = tryCatch(async (req, res, next) => {
-    const lighting = await Lighting.create(req.body)
+    const events = await Event.find({
+        user: req.user,
+        status: 'Approved'
+    })
+    let event = ''
+    console.log(events)
+    events.forEach(e => {
+        const endDate = new Date(e.end);
+        const today = new Date();
+
+        // Set today's time to 00:00:00 to compare only dates (optional)
+        today.setHours(0, 0, 0, 0);
+
+        if (endDate >= today) event = e._id
+    });
+    const lighting = await Lighting.create({ ...req.body, event })
     res.status(200).json({ success: true, data: lighting, msg: 'Lighting Details Stored Successfully' })
 })
 
